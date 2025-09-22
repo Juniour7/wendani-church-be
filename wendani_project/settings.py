b030b4e5858feb5a86e33ef55259b3e91522a3a3
Junior7 <giftmomanyi97@gmail.com>
2025-09-22 22:41:28 +0300

    feat: added blogg_app

--- wendani_project/settings.py
+++ wendani_project/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'church_app',
+    'blog_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
