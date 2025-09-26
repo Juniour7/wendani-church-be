151e943aa9fc01e45d24bc8aabf1babb92ae2393
Junior7 <giftmomanyi97@gmail.com>
2025-09-26 14:06:21 +0300

    fix:database set up completed and successful

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Blog, Author
-from .Serializers import BlogSerializer, AuthorSerializer
+from .serializers import BlogSerializer, AuthorSerializer
 from rest_framework import viewsets
 
 # Create your views here.
