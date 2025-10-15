8739b0ea04951f2545911cc7dc780b5c7d6c5dbf
Junior7 <giftmomanyi97@gmail.com>
2025-10-15 08:53:45 +0300

    feat:added temlates

--- wendani_project/settings.py
+++ wendani_project/settings.py
@@ -87,6 +87,23 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # you can create this folder if it doesn’t exist
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
 # -------------------------------------------------------------------
 # REST FRAMEWORK / JWT
 # -------------------------------------------------------------------
