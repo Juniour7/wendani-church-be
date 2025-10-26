512a2f9170ed101691a154b742d12c45cb4fc44b
Junior7 <giftmomanyi97@gmail.com>
2025-10-26 12:46:18 +0300

    feat:loaded .env file

--- wendani_project/settings.py
+++ wendani_project/settings.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # -------------------------------------------------------------------
 # BASIC SETTINGS
